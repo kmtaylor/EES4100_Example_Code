bebe48006f7cbb0fb57df296317b1646afbb75ab
Your Name <you@example.com>
2016-03-14 12:56:47 +1100

    Added getopt_long argument parsing

--- src/example_app.c
+++ src/example_app.c
@@ -1,8 +1,32 @@
 #include <stdio.h>
 #include <modbus-tcp.h>
+#include <unistd.h>
+#include <getopt.h>
 
-int main(void) {
+int main(int argc, char **argv) {
     modbus_t *ctx;
+    int option_index, c;
+
+    struct option long_options[] = {
+        { "count",      required_argument,  0, 'c' },
+        { "directive",  no_argument,        0, 'd' },
+        { 0 }
+    };
+
+    while (1) {
+        c = getopt_long(argc, argv, "c:d", long_options, &option_index);
+
+        if (c == -1) break;
+
+        switch (c) {
+            case 'c':
+                printf("Got count argument with value %s\n", optarg);
+                break;
+            case 'd':
+                printf("Got directive argument\n");
+                break;
+        }
+    }
 
     ctx = modbus_new_tcp("127.0.0.1", 1502);
 
